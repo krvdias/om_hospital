249769a3bcd94d598af6aea3c33018683a849d99
krvdias <vishan@kdenterprises.lk>
2025-10-23 17:23:55 +0530

    [DEV] add ondelete decorator for delete warning

--- models/patient.py
+++ models/patient.py
@@ -13,10 +13,18 @@ class HospitalPatient(models.Model):
     tag_ids = fields.Many2many('patient.tag', string="Tags")
     product_ids = fields.Many2many('product.product', string="Products")
 
-    def unlink(self):
+    # def unlink(self):
+    #     for rec in self:
+    #         domain = [('patient_id', '=', rec.id)]
+    #         appointments = self.env['hospital.appointment'].search(domain)
+    #         if appointments:
+    #             raise ValidationError(_("You can' delete the patient now." "\nAppointments existing for this patient:  %s" % rec.name))
+    #     return super().unlink() #if patient has assigned appointments restrict and show error for user
+
+    @api.ondelete(at_uninstall=False) #ondelete decorator using same as unlink
+    def _check_patient_appointments(self):
         for rec in self:
             domain = [('patient_id', '=', rec.id)]
             appointments = self.env['hospital.appointment'].search(domain)
             if appointments:
                 raise ValidationError(_("You can' delete the patient now." "\nAppointments existing for this patient:  %s" % rec.name))
-        return super().unlink() #if patient has assigned appointments restrict and show error for user
